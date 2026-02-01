8d028a7ff4c2cf5c2c38171a276a0e6042107e25
gunmack <jreedoy2002@yahoo.com>
2026-02-01 02:16:52 -0800

    added auth context profile and settings

--- front-end/src/app/profile/page.js
+++ front-end/src/app/profile/page.js
@@ -1,14 +1,28 @@
 "use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../context/AuthContext"; // adjust path
+import DropdownMenu from "@/components/dropdown";
 export default function Profile() {
+  const router = useRouter();
+  const { user } = useAuth(); // use context instead of local state
+
+  // redirect to auth page if not logged in
+  useEffect(() => {
+    if (!user) router.push("/auth");
+  }, [user, router]);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center sm:items-start">
-          <h1 className="text-5xl font-extrabold text-black dark:text-white sm:text-6xl">
-            Welcome to <span className="text-blue-600">Profile page</span>
-          </h1>
-        </div>
-      </main>
+    <div>
+      <DropdownMenu />
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <div className="flex flex-col items-center sm:items-start">
+            <h1 className="text-5xl font-extrabold text-black dark:text-white sm:text-6xl">
+              Welcome to <span className="text-blue-600">Profile page</span>
+            </h1>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
