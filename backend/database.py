08b8b0ce5dc68ba6df4218bfcd59908615411827
DerpyWasHere <seanchan@telus.net>
2026-02-01 04:29:10 -0800

    checkpoint, working baseline, can connect to db, need to manually input password and url

---
+++ backend/database.py
@@ -0,0 +1,94 @@
+import asyncio
+import contextlib
+import os
+import sqlalchemy
+import asyncpg
+from sqlalchemy import MetaData
+from typing import Annotated, Any
+from fastapi import FastAPI, Depends
+from collections.abc import AsyncIterator
+from sqlalchemy.ext.asyncio import (
+    AsyncConnection,
+    AsyncSession
+)
+
+convention = {
+    "ix": "ix_%(column_0_label)s", # index
+    "uq": "uq_%(table_name)s_%(column_0_name)s", # unique
+    "ck": "ck_%(table_name)s_%(constraint_name)s", # check
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s", # foreign key
+    "pk": "pk_%(table_name)s", # primary key
+}
+
+Base = sqlalchemy.orm.declarative_base()
+Base.metadata = MetaData(naming_convention=convention)
+
+class DatabaseSessionManager:
+    def __init__(self, db_url: str, engine_kwargs: dict[str, Any], check_db=True):
+        self._engine = sqlalchemy.ext.asyncio.create_async_engine(db_url, **engine_kwargs)
+        self._sessionmaker = sqlalchemy.ext.asyncio.async_sessionmaker(autocommit=False, bind=self._engine)
+        if check_db:
+            asyncio.run(DatabaseSessionManager.test_connection(db_url))
+
+    @staticmethod
+    async def test_connection(url: str):
+        try:
+            url = url.replace("+asyncpg", "")
+            print(url.replace("+asyncpg", ""))
+            conn = await asyncpg.connect(host='bedrock-db1.chacwaksqw4h.us-east-2.rds.amazonaws.com', user="bedrock",
+                                         database="postgres",
+                                         password="")
+            await conn.close()
+        except Exception as e:
+            raise Exception("fuck my baka chungus life")
+
+    async def close(self):
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        await self._engine.dispose()
+
+        self._engine = None
+        self._sessionmaker = None
+
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncConnection]:
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+
+        async with self._engine.begin() as connection:
+            try:
+                yield connection
+            except Exception:
+                await connection.rollback()
+                raise
+
+    @contextlib.asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._sessionmaker is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+
+        session = self._sessionmaker()
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+def setup():
+    global dbsessionmanager
+    dbsessionmanager = DatabaseSessionManager("postgresql+asyncpg://bedrock:<password>@bedrock-db1.chacwaksqw4h.us-east-2.rds.amazonaws.com:5432/postgres",
+                                              {"echo":True})
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    if dbsessionmanager._engine is not None:
+        await dbsessionmanager.close()
+
+async def _db_session():
+    async with dbsessionmanager.session() as session:
+        yield session
+
+DBSession = Annotated[AsyncSession, Depends(_db_session)]
--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,13 @@
 import logging
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+import database
 
 logging.basicConfig(level=logging.DEBUG)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
+database.setup()
 
 app = FastAPI(
-    lifespan=lifespan,
+    lifespan=database.lifespan,
     title="stevens sex dungeon",
     description=f'peanits',
     root_path="/api"
@@ -20,4 +17,44 @@ app = FastAPI(
 async def read_root():
     return {"message": "steeeeeeeeeeeeeeeeewen"}
 
+from fastapi import Request
+from fastapi.responses import JSONResponse
+import sqlalchemy
 
+@app.get("/test/{username}")
+async def get_user(
+    request: Request,
+    db_session: database.DBSession,
+    username: str
+):
+    print(username)
+    res = await db_session.scalar(
+        sqlalchemy.select(BedrockUser)
+        .where(BedrockUser.username == username)
+    )
+
+    return JSONResponse(res.to_dict())
+
+
+from database import Base
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    PrimaryKeyConstraint,
+    String,
+    Text
+)
+
+class BedrockUser(Base):
+    __tablename__ = "bedrock_user"
+
+    username = Column(String(255), primary_key=True, nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
+    u_token = Column(String(255))
+
+    def to_dict(self) -> dict:
+        return {
+            "username" : self.username,
+            "email" : self.email,
+            "u_token" : self.u_token
+        }
